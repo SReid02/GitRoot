365e6a1e091a4411f7b44ca39bcbcc146d66c04b
SReid02 <S102994@student.aiict.edu.au>
2022-06-07 18:39:12 -0700

    Adding Learning Mats

---
+++ Scripts/AZ-040T00/Lab 1/Learning Get Module Commands.ps1
@@ -0,0 +1,10 @@
+﻿Get-Module #will list the modules
+get-command -module *microsoft.powershell.management* #shows all the commands under powershell module.
+
+get-command *network*
+get-command *ping*
+get-command *active*
+
+Get-Alias -name erase #gets the name of alias's related to that name
+get-alias -Definition erase #gets the alias for specified item.
+New-Alias -Name List -Value Get-ChildItem #creates new alias 
