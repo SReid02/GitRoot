b2477c86b992d9dff67895f530ecf89fef2fc61f
SReid02 <S102994@student.aiict.edu.au>
2022-06-07 17:10:59 -0700

    Added learning

---
+++ Scripts/AZ-040T00/Lab 1/Learning Get member and properties.ps1
@@ -0,0 +1,5 @@
+﻿Get-Service -Name Spooler | Get-Member
+Get-Process -name notepad | Select-Object -Property name,priorityboostenabled
+
+(Get-Service -Name Spooler).StartType |Get-Member
+(Get-Service -name Spooler).StartType.value__
