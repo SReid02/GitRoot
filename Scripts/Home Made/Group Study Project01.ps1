f40d215a7451fc48164a6e81e9d22edcbd7ba56e
SReid02 <S102994@student.aiict.edu.au>
2022-07-12 17:06:49 +1000

    update for PS

---
+++ Scripts/Home Made/Group Study Project01.ps1
@@ -0,0 +1,14 @@
+﻿#Function for creating google searchs
+
+Function search-google {
+        #Setting query variable with basic google search URL
+        $query = 'https://www.google.com/search?q='
+        #% is an alias for "ForEachobject"
+        $args | ForEach-Object { $query = $query + "$_+" }
+        #
+        $url = $query.Substring(0, $query.Length - 1)
+        # 
+        start "$url"
+}
+#uses default web browser to run above funtion. Afterwards it navigates to google then you choose your search paramaters inbetween ** after serach-google command
+search-google *powershell meaning before {}*
