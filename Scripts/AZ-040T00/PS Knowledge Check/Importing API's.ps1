c9a13915693a6e5d8873a4bca469ae3b3982c619
Sreid02 <S102994@studentaiict.edu.au>
2022-07-17 23:50:37 -0700

    Class work for 18/07/22

---
+++ Scripts/AZ-040T00/PS Knowledge Check/Importing API's.ps1
@@ -0,0 +1,2 @@
+﻿$SwarsPers5 = Invoke-WebRequest -uri "https://swapi.dev/api/people/5/"
+$SwarsPers5.Content |ConvertFrom-Json
---
+++ Scripts/AZ-040T00/PS Knowledge Check/TEST ad user.ps1
@@ -0,0 +1,15 @@
+﻿Import-Csv C:\newuser.csv
+
+$userfromCSV = Import-Csv C:\newuser.csv
+
+foreach ($aduser in $userfromCSV){
+$splat = @{
+GivenName = $aduser.firstname 
+surname = $aduser.Lastname
+Department = $aduser.department 
+City = $aduser.city
+name = $aduser.firstname + ' ' + $aduser.lastname
+}
+
+if ($aduser.department -eq 'it'){}
+if ($aduser.city -eq 'london')}
