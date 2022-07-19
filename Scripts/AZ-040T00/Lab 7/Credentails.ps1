87af26aa41f3750cbb3076b6d153745b4c549199
Sreid02 <S102994@student.aiict.edu.au>
2022-07-18 17:24:51 -0700

    update 19072022

--- Scripts/AZ-040T00/Lab 7/Credentails.ps1
+++ Scripts/AZ-040T00/Lab 7/Credentails.ps1
@@ -1,1 +1,10 @@
-﻿
+﻿$cred = Get-Credential
+
+
+Get-ADUser -Filter * |
+            Out-GridView -OutputMode Single -Title "Select the user to reset their password" |
+            ForEach-Object {
+                $cred = Get-Credential -UserName $_.name -Message "Please enter new password"
+                Set-ADAccountPassword -NewPassword $cred.password -Reset -identity $_
+            }
+            
