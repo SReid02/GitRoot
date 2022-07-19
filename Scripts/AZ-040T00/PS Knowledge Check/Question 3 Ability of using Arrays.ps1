c394b83e1dfcb684a7de8c88049a6007712efd75
SReid02 <S102994@student.aiict.edu.au>
2022-07-20 09:00:06 +1000

    update for PS

--- Scripts/AZ-040T00/PS Knowledge Check/Question 3 Ability of using Arrays.ps1
+++ Scripts/AZ-040T00/PS Knowledge Check/Question 3 Ability of using Arrays.ps1
@@ -1,1 +1,14 @@
-﻿
+﻿#Q3.1 Write a function that will take an array of integers and will return the value of the smallest and largest added together
+
+function add-smalltolarge {
+    param (
+        [Int32[]]$NumberArray  
+           
+    )
+    $LargestNo = Measure-Object -Maximum
+    $SmallestNo = Measure-Object -Maximum
+    Return $LargestNo + $SmallestNo
+    
+}
+
+add-smalltolarge -NumberArray '241,123,667,134,7890,12,3'
