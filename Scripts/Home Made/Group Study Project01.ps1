721da3df0573570c5a90afd19803e8ff50a54aad
SReid02 <S102994@student.aiict.edu.au>
2022-07-20 00:57:57 +1000

    update for PS

--- Scripts/Home Made/Group Study Project01.ps1
+++ Scripts/Home Made/Group Study Project01.ps1
@@ -5,9 +5,9 @@ Function search-google {
         $query = 'https://www.google.com/search?q='
         #% is an alias for "ForEachobject"
         $args | ForEach-Object { $query = $query + "$_+" }
-        #
-        $url = $query.Substring(0, $query.Length - 1)
-        # 
+        # -1 removes the + sign at the end
+        $url = $query.Substring(0, $query.Length -1)
+        # start is used to open defult web browser
         start "$url"
 }
 #uses default web browser to run above funtion. Afterwards it navigates to google then you choose your search paramaters inbetween ** after serach-google command
